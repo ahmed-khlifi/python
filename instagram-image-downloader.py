95bc60e1f48dff856db71e790d8107174a0ed463
Ahmed Khlifi <47308272+ahmed-khlifi@users.noreply.github.com>
2019-12-30 21:14:06 +0000

    Update instagram-image-downloader.py

--- instagram-image-downloader.py
+++ instagram-image-downloader.py
@@ -3,21 +3,19 @@ from bs4 import BeautifulSoup
 import random
 
 url = input('The URL > ')  # default is downloading an image ..
-name = random.randrange(1, 50)
-
+name = random.randrange(1, 50) # give a random name to saved image 
 
 def download():
     print('[/] just wait ..')
     try:
-        source_code = urllib.request.urlopen(url)  # get the source code
+        source_code = urllib.request.urlopen(url)  # url
         soup = BeautifulSoup(source_code, "html.parser")  # get source code
-        for link in soup.find_all('meta', {'property': 'og:image'}):  # get the url
+        for link in soup.find_all('meta', {'property': 'og:image'}):  # find image url
             content = link.get('content')
-            full_name = str(name) + '.jpg'
+            full_name = str(name) + '.jpg' #save the downloaded file
             urllib.request.urlretrieve(content, full_name)
-            print('[+] saved as ' + full_name)
+            print('[+] saved as ' + full_name) # image saved in the directory of the script .
     except urllib.request.HTTPError:
-        print('Public Accounts only')
-
-
+        print('Public Accounts only or private ones you have access to them') #work on public accounts or privat if u following it
+        
 download()
