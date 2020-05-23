462d4cedd85280adf179e702e30965838ecc5cdd
Ahmed Khlifi <47308272+ahmed-khlifi@users.noreply.github.com>
2020-05-23 15:29:35 +0100

    Update mailer.py

--- mailer.py
+++ mailer.py
@@ -4,6 +4,6 @@ message = "this email sent via python script"
 #send via gmail server
 server = smtplib.SMTP("smtp.gmail.com", 587) # https://support.google.com/a/answer/176600?hl=en
 server.starttls() #start encryption
-server.login("sender@email.com", "sender_password") # the
+server.login("sender@email.com", "sender_password")
 server.sendmail("senderr@email.com", "recipient@email.com", message)
 # in order to send email without problems you must turn this on > https://myaccount.google.com/lesssecureapps
