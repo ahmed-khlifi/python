a0c7b1fd8b1796c2b997826e2473f55cc806a671
ahmed-khlifi <ahmedkh3lifi@gmail.com>
2020-08-20 14:31:29 +0100

    coloring prompt

--- translator.py
+++ translator.py
@@ -1,15 +1,16 @@
 from googletrans import Translator
-
+from colorama import Fore
 def main():
-    text = input("\033[1;32;40m Translate your text :")
-    dest = input('print language :')
+    text = input(Fore.BLUE + "Translate your text :")
+    dest = input(Fore.BLUE + 'print language :')
     print(p(text, dest))
 
 def p(text , dest):
     t = Translator()
-    detect = t.detect(text)
-    return f" the language you wrote is  {detect.confidence*100}% {detect.lang}"
-    #return t.translate(text, dest=dest).text
+    detect = Fore.WHITE + f" the language you wrote is  {t.detect(text).confidence*100}% {t.detect(text).lang}"
+    translated = t.translate(text, dest=dest).text
+    message = f"{detect} \n translation : \n {translated}"
+    return (message) 
 
 if __name__ == "__main__":
     main()
