9a343b690d827ec3a9243af9f9f3bd9f87211aae
ahmed-khlifi <ahmedkh3lifi@gmail.com>
2020-08-20 14:55:18 +0100

    handle invalid language error

--- translator.py
+++ translator.py
@@ -1,16 +1,22 @@
 from googletrans import Translator
 from colorama import Fore
+from sys import exit
+
 def main():
     text = input(Fore.BLUE + "Translate your text :")
     dest = input(Fore.BLUE + 'print language :')
     print(p(text, dest))
 
 def p(text , dest):
-    t = Translator()
-    detect = Fore.WHITE + f" the language you wrote is  {t.detect(text).confidence*100}% {t.detect(text).lang}"
-    translated = t.translate(text, dest=dest).text
-    message = f"{detect} \n translation : \n {translated}"
-    return (message) 
+    try:
+        t = Translator()
+        detect = Fore.WHITE + f" the language you wrote is  {t.detect(text).confidence*100}% {t.detect(text).lang}"
+        translated = t.translate(text, dest=dest).text
+        message = f"{detect} \n translation : \n {translated}"
+        return (message) 
+    except ValueError:
+        print("write a valide language please.")
+        main()
 
 if __name__ == "__main__":
     main()
