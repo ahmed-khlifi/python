4480c4f247e3806327c89c6d3a5033d90d4d89f2
ahmed-khlifi <ahmedkh3lifi@gmail.com>
2020-08-23 02:48:16 +0100

    listning to changes

--- followers_number.py
+++ followers_number.py
@@ -6,31 +6,27 @@ import sqlite3
 def w(info):
     with open("info.txt", "w") as fw:
         fw.writelines(info)
-check = 'script run for the first time'
+
 try :
 	with open("info.txt", "r") as fr:
 	    check = fr.read()
 except FileNotFoundError:
-	pass
+	check = 'script run for the first time'
 	
 #change user name
 usr = "khelifi_ahmed_"
 URL = "https://www.instagram.com/{}/"
-rq = requests.get(URL.format(usr))
-soup = BeautifulSoup(rq.text, "html.parser")
-for l in soup.find_all('meta', {'property':'og:description'}):
-    info = l['content'].split('-',1)[0]
-    w(info)
 
-if check != info:
-    print(f''' 
-status updated :
-old : {check}
-new : {info}
-    ''')
-    w(info)
-else:
-    print("nothing changed")
+print("listening for changes ..")
+while True:
+	rq = requests.get(URL.format(usr))
+	soup = BeautifulSoup(rq.text, "html.parser")
+	for l in soup.find_all('meta', {'property':'og:description'}):
+		info = l['content'].split('-',1)[0]
+		if check != info:
+			print(f"status updated :\n old : {check}\n new : {info} ")
+			w(info)
+			break
+
 
 #output : X Followers, X Following, X Posts 
-
