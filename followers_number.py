faa5369c4113a789251b9c6a03b0286051fc7735
ahmed-khlifi <ahmedkh3lifi@gmail.com>
2020-08-23 23:17:15 +0100

    reset

--- followers_number.py
+++ followers_number.py
@@ -5,28 +5,31 @@ import sqlite3
 
 def w(info):
     with open("info.txt", "w") as fw:
-        fw.writelines(info)
-
+        fw.write(info)
+check = 'script run for the first time'
 try :
 	with open("info.txt", "r") as fr:
 	    check = fr.read()
 except FileNotFoundError:
-	check = 'script run for the first time'
+	pass
 	
 #change user name
 usr = "khelifi_ahmed_"
 URL = "https://www.instagram.com/{}/"
+rq = requests.get(URL.format(usr))
+soup = BeautifulSoup(rq.text, "html.parser")
+for l in soup.find_all('meta', {'property':'og:description'}):
+    info = l['content'].split('-',1)[0]
+    w(info)
 
-print("listening for changes ..")
-while True:
-	rq = requests.get(URL.format(usr))
-	soup = BeautifulSoup(rq.text, "html.parser")
-	for l in soup.find_all('meta', {'property':'og:description'}):
-		info = l['content'].split('-',1)[0]
-		if check != info:
-			print(f"status updated :\n old : {check}\n new : {info} ")
-			w(info)
-			break
-
+if check != info:
+    print(f''' 
+status updated :
+old : {check}
+new : {info}
+    ''')
+    w(info)
+else:
+    print("nothing changed")
 
 #output : X Followers, X Following, X Posts 
