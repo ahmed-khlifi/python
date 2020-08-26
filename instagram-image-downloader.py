d06b2e7df6b67dbbcabee128477a94e5faa6450f
ahmed-khlifi <ahmedkh3lifi@gmail.com>
2020-08-26 01:24:53 +0100

    use image link as argument

--- instagram-image-downloader.py
+++ instagram-image-downloader.py
@@ -1,21 +1,24 @@
 import urllib.request
 from bs4 import BeautifulSoup
 import random
+from sys import argv
+import os
 
-url = input('The URL > ')  # default is downloading an image ..
+if len(argv) != 2:
+        print(f"usage : {argv[0]} picture_link")
+        os._exit(1)
 name = random.randrange(1, 50) # will change [TODO]
 
-def download():
-    print('[/] just wait ..')
-    try:
-        source_code = urllib.request.urlopen(url)  # url
-        soup = BeautifulSoup(source_code, "html.parser")  # get source code
-        for link in soup.find_all('meta', {'property': 'og:image'}):  # find image url
-            content = link.get('content')
-            full_name = str(name) + '.jpg' #save the downloaded file
-            urllib.request.urlretrieve(content, full_name)
-            print('[+] saved as ' + full_name) # image saved in the directory of the script .
-    except urllib.request.HTTPError:
-        print('Public Accounts only or private ones you have access to them') #work on public accounts or privat if u following it
-        
-download()
+
+print('[/] just wait ..')
+try:
+    source_code = urllib.request.urlopen(argv[1])  # url
+    soup = BeautifulSoup(source_code, "html.parser")  # get source code
+    for link in soup.find_all('meta', {'property': 'og:image'}):  # find image url
+        content = link.get('content')
+        full_name = str(name) + '.jpg' #save the downloaded file
+        urllib.request.urlretrieve(content, full_name)
+        print('[+] saved as ' + full_name) # image saved in the directory of the script .
+except urllib.request.HTTPError:
+    print('error ') 
+
